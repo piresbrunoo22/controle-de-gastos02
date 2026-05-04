e833b46952f254bc04eccea5e1872ff56acc8c9f
piresbrunoo22 <brunooliveirapires02@gmail.com>
2026-05-04 19:49:50 -0300

    primeiro git

---
+++ src/main/java/br/com/controledegastos02/ControleDeGastos02Application.java
@@ -0,0 +1,13 @@
+package br.com.controledegastos02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeGastos02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeGastos02Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/controledegastos02/ControleDeGastos02ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controledegastos02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeGastos02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
